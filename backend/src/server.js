8b157a357d597e9365140c1735fe6f615354bf27
Abhiraj Patel <abhi0808raj@gmail.com>
2025-08-27 19:16:48 +0530

    Added .env to hide the url

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 import express from "express"
 import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app=express();
+
+connectDB();
 
 app.use("/api/notes", notesRoutes);
 
@@ -9,6 +15,3 @@ app.listen(5001, ()=>{
     console.log("Server Started on PORT: 5001");
 });
 
-
-
-// mongodb+srv://abhi0808raj:J4SsNMRrkWZ33Bxg@mern-thinkboard.vdcikiw.mongodb.net/?retryWrites=true&w=majority&appName=Mern-Thinkboard
