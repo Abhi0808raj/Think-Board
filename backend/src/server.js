b3e4bef457f190be3fe8ebe0b851c3263fca8661
Abhiraj Patel <abhi0808raj@gmail.com>
2025-08-29 02:27:14 +0530

    Home Page Completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
 
 dotenv.config();
  
@@ -11,9 +12,12 @@ const app=express();
 const PORT=process.env.PORT ||5001;
 
 //middleware
+app.use(cors({
+    origin:"http://localhost:5173",
+}));
+
 app.use(express.json()); //Middleware to parse JSON bodies req.body
 app.use(rateLimiter);  //Middleware to do rate limiting
-
 
 app.use("/api/notes", notesRoutes);
 
